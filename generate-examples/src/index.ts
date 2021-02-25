5852c681c4fdd1768a469a97564a8b58a06d8c0f
Christian Paquin <cpaquin@microsoft.com>
2021-02-25 03:47:31 -0500

    Updates.

--- generate-examples/src/index.ts
+++ generate-examples/src/index.ts
@@ -1,12 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Command } from 'commander';
+import { Option, Command } from 'commander';
 import fs from 'fs';
 import got from 'got';
 import jose, { JWK } from 'node-jose';
 import pako, { deflate, deflateRaw } from 'pako';
 import QrCode, { QRCodeSegment } from 'qrcode';
-import issuerPrivateKeys from './config/issuer.jwks.private.json';
-import issuer2PrivateSigningKey from './config/issuer2.jwks.private.json';
 
 
 const ISSUER_URL = process.env.ISSUER_URL || 'https://smarthealth.cards/examples/issuer' ;
@@ -127,12 +125,12 @@ function createHealthCardJwsPayload(fhirBundle: Bundle): Record<string, unknown>
       '@context': ['https://www.w3.org/2018/credentials/v1'],
       type: [
         'VerifiableCredential',
-        'https://smarthealth.cards#health-card',
+        _healthCardUri,
         'https://smarthealth.cards#immunization',
         'https://smarthealth.cards#covid19',
       ],
       credentialSubject: {
-        fhirVersion: '4.0.1',
+        fhirVersion: '4.0.1', // TODO: which fhirVersion is ok?
         fhirBundle,
       },
     },
@@ -149,14 +147,14 @@ async function createHealthCardFile(jwsPayload: Record<string, unknown>): Promis
 
 const SMALLEST_B64_CHAR_CODE = 45; // "-".charCodeAt(0) === 45
 const toNumericQr = (jws: string): QRCodeSegment[] => [
-  { data: 'shc:/', mode: 'byte' },
+  { data: _qrHeader, mode: 'byte' },
   {
     data: jws
       .split('')
       .map((c) => c.charCodeAt(0) - SMALLEST_B64_CHAR_CODE)
       .flatMap((c) => [Math.floor(c / 10), c % 10])
       .join(''),
-    mode: 'numeric',
+    mode: _qrMode,
   },
 ];
 
@@ -232,7 +230,18 @@ async function generate(options: { outdir: string, testcase:string }) {
 
 const program = new Command();
 program.option('-o, --outdir <outdir>', 'output directory');
-program.option('-t, --testcase <testcase>', 'test case to generate');
+program.addOption(new Option('-t, --testcase <testcase>', 'test case to generate').choices([
+  'invalid_deflate',
+  'invalid_jws_format',
+  'invalid_issuer_url',
+  'wrong_qr_header',
+  'wrong_qr_mode',
+  'wrong_issuer_key',
+  'wrong_issuer_curve_key',
+  'wrong_issuer_kid_key',
+  'wrong_issuer_kty_key',
+  'invalid_healthcard_uri'
+]));
 program.parse(process.argv);
 
 interface Options {
@@ -248,7 +257,15 @@ const _deflateFunction = options.testcase == 'invalid_deflate' ? pako.deflateRaw
 const _jwsFormat = options.testcase == 'invalid_jws_format' ? 'flattened' : 'compact';
 const _issuerUrlSuffix = options.testcase == 'invalid_issuer_url' ? 'invalid_url' : '';
 const _qrHeader = options.testcase == 'wrong_qr_header' ? 'shc:' : 'shc:/';
-const _issuerSigningKey = options.testcase == 'wrong_issuer_key' ? issuerPrivateKeys.keys[0] : issuer2PrivateSigningKey;
+const _qrMode = options.testcase == 'wrong_qr_mode' ? 'byte' : 'numeric';
+const _issuerKeyFile = './src/config/' + 
+  (options.testcase == 'wrong_issuer_key' ? 'issuer2.jwks.private.json' : 
+    (options.testcase == 'wrong_issuer_curve_key' ? 'issuer_wrong_curve.jwks.private.json' : 
+      (options.testcase == 'wrong_issuer_kid_key' ? 'issuer_wrong_kid.jwks.private.json' : 
+        (options.testcase == 'wrong_issuer_kty_key' ? 'issuer_wrong_kty.jwks.private.json' :
+          'issuer.jwks.private.json'))));
+const _issuerSigningKey = JSON.parse(fs.readFileSync(_issuerKeyFile, 'utf-8'));
+const _healthCardUri = options.testcase == 'invalid_healthcard_uri' ? 'https://smarthealth.cards#health-card' : 'https://smarthealth.cards#wrong-health-card';
 
 if (options.outdir) {
   generate(options);
